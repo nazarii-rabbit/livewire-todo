c65fb33ee95a73b53a42c1a4e1d055f43e77f485
Nazarii Rabbit <rabbit@jetmonsters.me>
2024-05-22 10:10:23 +0300

    add Todo page

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,7 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+    Route::get('/todo-list', function () {
+        return view('pages.todo');
+    })->name('todo-list');
 });
